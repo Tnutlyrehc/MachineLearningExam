2373fb1074881297c22075f680ba308072c67a54
Tnutlyrehc <chrla15@student.sdu.dk>
2022-01-23 16:12:35 +0100

    .

--- main.py
+++ main.py
@@ -10,20 +10,37 @@ import keras
 from PIL import Image
 import glob
 import os
+import random
 
 # Assuming we're handed a CSV
 labels_csv = pd.read_csv('Data\DIDA_12000_String_Digit_Labels.csv', encoding='utf-8', delimiter=(','))
 
-filelist = []
+#Getting the 0 to 9 data
+numblist = []
 path = 'Data/10000'
 
+#Getting the images in the subfolders
 for root, dirs, files in os.walk(path):
     for file in files:
         # append the file name to the list
-        filelist.append(os.path.join(root, file))
+        numblist.append(os.path.join(root, file))
 
 # print all the file names
 # for name in filelist:
 #    print(name)
 
-print(len(filelist))
+print(len(numblist))
+
+#Split the images into 10 arrays (1000 in each)
+splits = np.array_split(numblist, 10)
+
+for array in splits:
+    print(list(array))
+
+print(len(splits))
+
+
+#Getting the DIDA CCDY data
+path_dida = 'Data/DIDA_1'
+images = glob.glob(random.choice(path_dida))
+random_image = random.choice(images)
