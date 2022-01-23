4101416961d67030fbbe1968d8e746cd736bb6bf
Tnutlyrehc <chrla15@student.sdu.dk>
2022-01-23 18:07:35 +0100

    .

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ from PIL import Image
 import glob
 import os
 import random
+import os
+import shutil
+import splitfolders
+
+
 
 # Assuming we're handed a CSV
-labels_csv = pd.read_csv('Data\DIDA_12000_String_Digit_Labels.csv', encoding='utf-8', delimiter=(','))
-
+df_labels = pd.read_csv('Data\DIDA_12000_String_Digit_Labels.csv', encoding='utf-8', delimiter=(','))
+"""
 #Getting the 0 to 9 data
 numblist = []
 path = 'Data/10000'
@@ -24,23 +29,7 @@ for root, dirs, files in os.walk(path):
     for file in files:
         # append the file name to the list
         numblist.append(os.path.join(root, file))
-
-# print all the file names
-# for name in filelist:
-#    print(name)
-
 print(len(numblist))
-
-#Split the images into 10 arrays (1000 in each)
-splits = np.array_split(numblist, 10)
-
-for array in splits:
-    print(list(array))
-
-print(len(splits))
+"""
 
 
-#Getting the DIDA CCDY data
-path_dida = 'Data/DIDA_1'
-images = glob.glob(random.choice(path_dida))
-random_image = random.choice(images)
