2cd2ca8bbb79ef34663d857633db5b98fbc26660
Tnutlyrehc <chrla15@student.sdu.dk>
2022-01-24 14:45:00 +0100

    .

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from keras.preprocessing import image
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Input, Dropout, Conv2D, MaxPool2D, GlobalAveragePooling2D, Concatenate
 from tensorflow.keras import regularizers
-path = 'C:/Users/felix/Documents/_FWM/Master/Semester 3/Applied Machine Learning/Exam/data'
+path = 'data'
 
 # creating the labels from CSV file
 labels = pd.read_csv(path + '/DIDA_12000_String_Digit_Labels.csv', names=['Index', 'Label'])
@@ -50,6 +50,7 @@ print('Year \n', labels['Y'].value_counts())
 # reading the data to a list
 raw_imgs = []
 
+
 for i in os.listdir(path + '/original_data'):
     current_image = image.load_img(os.path.join(path + '/original_data', i))
     raw_imgs.append([current_image, i])
@@ -67,7 +68,7 @@ print(y_index_train)
 #y_train = labels['Index' == X_train]
 # writing it to the three directories
 def write_to_dir (data, type):
-    path = 'C:/Users/felix/Documents/_FWM/Master/Semester 3/Applied Machine Learning/Exam/data'  + '/' + type
+    path = 'data'  + '/' + type
     for i in range(len(data)):
         image.save_img(os.path.join(path, data[i][1]), data[i][0])
 write_to_dir(X_test, 'test')
---
+++ visualizations.py
@@ -0,0 +1,14 @@
+from matplotlib import image
+from matplotlib import pyplot
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+data = image.imread('Data/train/258.jpg')
+print(data.dtype)
+print(data.shape)
+# display the array of pixels as an image
+pyplot.imshow(data)
+pyplot.show()
