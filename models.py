12e8e7b3d5c12383b76034946ea17291e7154499
h152056 <petter_salberg@hotmail.com>
2022-01-25 17:06:25 +0100

    build_random_forest, build_boosting and merge_rf_boost

--- models.py
+++ models.py
@@ -1,15 +1,19 @@
 """
 Skeleton model(s) to make model creation easier
+- Random Forest
+- Boosting
+- Wrapper of Random Forest and Boosting
 """
 from tensorflow.keras.layers import *
 from tensorflow.keras import models
 from tensorflow.keras import optimizers
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
 
-# For Neural Networks, sequential
+# For Neural Networks, sequential (From lecture slides "optimization and regularization"
 def build_model_neural_sequential(
-        size, # number of layers
-        activation, # what activation function to use in the Dense layers
+        size,  # number of layers
+        activation,  # what activation function to use in the Dense layers
         weight_regularizer,
         dropout,
         batch_norm,
@@ -19,7 +23,7 @@ def build_model_neural_sequential(
     lay = [Flatten(input_shape=(150, 84))]  # Input layer
 
     numb_layers = size
-    numb_neurons = size ** 2 * 4 # Can this be a different value?
+    numb_neurons = size ** 2 * 4  # Can this be a different value?
 
     for i in range(numb_layers):
         lay.append(Dense(
@@ -47,21 +51,64 @@ def build_model_neural_sequential(
         model.summary()
         return model
 
+# Random forest, there are more parameters to consider in the future for optimization in all the models.
+def build_random_forest(number_estimators,
+                        criterion,
+                        max_depth,
+                        min_samples_split,
+                        min_samples_leaf,
+                        max_features,
+                        bootstrap,
+                        random_state
+                        ):
+    rf_model = RandomForestClassifier(n_estimators=number_estimators,
+                                      criterion=criterion,
+                                      max_depth=max_depth,
+                                      min_samples_split=min_samples_split,
+                                      min_samples_leaf=min_samples_leaf,
+                                      max_features=max_features,
+                                      bootstrap=bootstrap,
+                                      random_state=random_state)
+
+    return rf_model
+
+# Gradient Boosting Classifier, what about other boosting`s? XGBoost, AdaBoost etc.
+def build_boosting(loss,
+                   learning_rate,
+                   n_estimators,
+                   subsample,
+                   ):
+    gb_model = GradientBoostingClassifier(loss=loss,
+                                          learning_rate=learning_rate,
+                                          n_estimators=n_estimators,
+                                          subsample=subsample)
+
+    return gb_model
+
+# Wrapping the Random Forest and Boosting models
+# Boosting first, then take the final adjusted dataset and insert it into a Random Forest Model:
+# After training the boosting model, call this function and use the returned model to train the random forest??
+def merge_rf_boost(
+        rf_model,
+        b_model
+        ):
+    b_model.estimators_ += rf_model.estimators_
+    b_model.n_estimators = len(b_model.estimators_)
+
+    return b_model
+
+
 def build_model_CC():
-
-
-
     return modelCC
 
-def build_model_D():
 
+def build_model_D():
     return modelD
 
-def build_model_Y():
 
+def build_model_Y():
     return modelY
 
+
 def merged_CCDY():
-
-     return modelCCDY
-
+    return modelCCDY
