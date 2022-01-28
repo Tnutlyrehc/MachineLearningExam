bceb6df583c4f02626f1a13415639bdcea2a9ce6
felixhin21 <felixhindrichs@web.de>
2022-01-28 18:13:08 +0100

    better optics

--- training_plot.py
+++ training_plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 history = np.load('my_history.npy', allow_pickle='True').item()
 
-def plot_loss_acc(history):
+def plot_loss_acc(history, filename):
     history_dict = history
     loss_values = history_dict['loss']
     val_loss_values = history_dict['val_loss']
@@ -10,12 +10,15 @@ def plot_loss_acc(history):
     val_acc = history_dict['val_accuracy']
     epochs = range(1, len(loss_values) + 1)
 
-    fig, axs = plt.subplots(2)
+    fig, axs = plt.subplots(nrows=1, ncols=2)
     axs[0].plot(epochs, loss_values, 'bo', label='Training loss')
     axs[0].plot(epochs, val_loss_values, 'b', label='Validation loss')
     axs[0].set_title('Training and validation loss')
+    axs[0].legend()
     axs[1].plot(epochs, train_acc, 'ro', label='Training accuracy')
     axs[1].plot(epochs, val_acc, 'r', label='Validation accuracy')
     axs[1].set_title('Training and validation accuracy')
-    plt.show()
-plot_loss_acc(history)
+    axs[1].legend()
+    fig.set_size_inches(8,5)
+    plt.savefig('plots/' + filename)
+plot_loss_acc(history, 'CC_overfitted.jpg')
