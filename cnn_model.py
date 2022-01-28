1794d1b596878672cf691aa04e453c68f154354f
h152056 <petter_salberg@hotmail.com>
2022-01-28 16:04:27 +0100

    updated cnn_model.py

--- cnn_model.py
+++ cnn_model.py
@@ -49,30 +49,55 @@ def build_cnn_model_resnet(ccmodel, dmodel, ymodel):
     CCMODEL = ccmodel
     DMODEL = dmodel
     YMODEL = ymodel
-    edges_layer = conv(input_layer, filters=32, kernel_size=(5, 5))
-    x = conv(edges_layer)
-    x = residualblock(x)
-    x = MaxPooling2D(x)
-    x = Flatten()(x)
-    x = Dense(256)(x)
-    x = BatchNormalization()(x)
-    x = ReLU()(x)
-    x = Dropout(0.4)(x)
-    x = Dense(1 if CCMODEL else 11, activation='sigmoid' if CCMODEL else 'softmax')(x)
-
     opt = Adam(learning_rate=0.001)
+
     if CCMODEL:
-        cc_model = Model(input_layer, x, name='CC model')
-        cc_model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['binary_accuracy'])
+        edges_layer = conv(input_layer, filters=32, kernel_size=(5, 5))
+        x = conv(edges_layer)
+        x = residualblock(x)
+        x = MaxPooling2D(x)
+        x = Flatten()(x)
+        x = Dense(256)(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = Dropout(0.4)(x)
+        x = Dense(1, activation='sigmoid')(x)
+
+        cc_model = Model(input_layer, x, name='modelCC')
+        cc_model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
         cc_model.summary()
         return cc_model
+
     elif DMODEL:
-        d_model = Model(input_layer, x, name='D model')
+        edges_layer = conv(input_layer, filters=32, kernel_size=(5, 5))
+        x = conv(edges_layer)
+        x = residualblock(x)
+        x = MaxPooling2D(x)
+        x = Flatten()(x)
+        x = Dense(256)(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = Dropout(0.4)(x)
+        x = Dense(5, activation='softmax')(x)
+
+        d_model = Model(input_layer, x, name='modelD')
         d_model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         d_model.summary()
         return d_model
+
     elif YMODEL:
-        y_model = Model(input_layer, x, name='Y model')
+        edges_layer = conv(input_layer, filters=32, kernel_size=(5, 5))
+        x = conv(edges_layer)
+        x = residualblock(x)
+        x = MaxPooling2D(x)
+        x = Flatten()(x)
+        x = Dense(256)(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = Dropout(0.4)(x)
+        x = Dense(10, activation='softmax')(x)
+
+        y_model = Model(input_layer, x, name='modelY')
         y_model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         y_model.summary()
         return y_model
@@ -82,26 +107,43 @@ def build_cnn_model_default(ccmodel, dmodel, ymodel):
     CCMODEL = ccmodel
     DMODEL = dmodel
     YMODEL = ymodel
-    input = input_layer
-    x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(input)
-    x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(x)
-    x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(x)
-    x = Flatten()(x)
-    x = Dense(1 if CCMODEL else 11, activation='sigmoid' if CCMODEL else 'softmax')(x)
-
     opt = Adam(learning_rate=0.001)
+
     if CCMODEL:
-        cc_model = Model(input, x, name='CC model')
-        cc_model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['binary_accuracy'])
+        input = input_layer
+        x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(input)
+        x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(x)
+        x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(x)
+        x = Flatten()(x)
+        x = Dense(1, activation='sigmoid')(x)
+
+        cc_model = Model(input, x, name='modelCC')
+        cc_model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
         cc_model.summary()
         return cc_model
+
     elif DMODEL:
-        d_model = Model(input, x, name='D model')
+        input = input_layer
+        x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(input)
+        x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(x)
+        x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(x)
+        x = Flatten()(x)
+        x = Dense(5, activation='softmax')(x)
+
+        d_model = Model(input, x, name='modelD')
         d_model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         d_model.summary()
         return d_model
+
     elif YMODEL:
-        y_model = Model(input, x, name='Y model')
+        input = input_layer
+        x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(input)
+        x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(x)
+        x = Conv2D(filters=32, padding='same', kernel_size=(3, 3))(x)
+        x = Flatten()(x)
+        x = Dense(10, activation='softmax')(x)
+
+        y_model = Model(input, x, name='modelY')
         y_model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         y_model.summary()
         return y_model
